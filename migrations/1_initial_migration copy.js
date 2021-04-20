0b570b90e43d624f149a34a7d07ae6556a3bf2c5
Thanwa J <thanwa.jdr@gmail.com>
2021-04-20 14:18:20 +0700

    remove copy

--- migrations/1_initial_migration copy.js
+++
@@ -1,6 +0,0 @@
-const Migrations = artifacts.require("Migrations");
-
-module.exports = function (deployer, network, addresses) {
-  const [admin] = addresses;
-  deployer.deploy(Migrations, { from: admin });
-};
